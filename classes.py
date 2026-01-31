09d3024a0d801beb0819ce97f574647d69839b1a
Roman Kremeshniy <sokems@mail.ru>
2026-01-31 10:59:54 +0500

    Создал классы

---
+++ classes.py
@@ -0,0 +1,67 @@
+
+
+class Thing:
+    """
+    Класс содержит в себе следующие параметры:
+    - название (name);
+    - процент защиты (percent_defense);
+    - атаку (attack);
+    - жизнь (hp);
+
+    Это могут быть предметы одежды,
+    магические кольца, всё что угодно)
+    """
+
+    def __init__(self, name, percent_defense, attack_damage, hp):
+        self.name = name
+        self.percent_defense = percent_defense
+        self.attack_damage = attack_damage
+        self.hp = hp
+
+
+class Person:
+    """
+    Класс, содержащий в себе следующие параметры:
+    - Имя, кол-во hp/жизней, базовую атаку, базовый процент защиты;
+    - Метод, принимающий на вход список вещей set_things(things);
+    - Метод вычитания жизни на основе входной атаки take_damage(attack_damage);
+    """
+
+    def __init__(self, name, percent_defense, attack_damage, hp):
+        self.name = name
+        self.hp = hp
+        self.attack_damage = attack_damage
+        self.percent_defense = percent_defense
+
+    def set_things(self, things):
+        """Метод принимает на вход список вещей и меняет базовые параметры"""
+        for thing in things:
+            self.hp += thing.hp
+            self.percent_defense += thing.percent_defense
+            self.attack_damage += thing.attack_damage
+
+    def take_damage(self, attack_damage):
+        """Метод принимает на вход наносимый урон и вычитает жизни"""
+        damage = attack_damage - attack_damage * self.percent_defense
+        self.hp -= damage
+
+
+class Paladin(Person):
+    """
+    Класс наследуется от персонажа,
+    при этом количество присвоенных жизней и процент защиты умножается на 2;
+    """
+
+    def __init__(self):
+        self.hp *= 2
+        self.percent_defense *= 2
+
+
+class Warrior(Person):
+    """
+    Класс наследуется от персонажа,
+    при этом атака умножается на 2.
+    """
+
+    def __init__(self):
+        self.attack_damage *= 2
